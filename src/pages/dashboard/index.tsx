697177b1c6024075ae89f2a560b6bb9f4cdd8835
ProgramaConPid <programaconpid@gmail.com>
2025-10-02 13:52:41 -0500

    Functions get and post using axios

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,10 +2,14 @@ import { useRouter } from "next/router";
 import { UserStore } from "@/helpers/utils";
 import { usersRegistered } from "@/helpers/utils";
 import { useState } from "react";
+import { createComment, getComments } from "@/services/comments";
 
 export default function Dashboard() {
   const router = useRouter();
-  const [comments, setComments] = useState([])
+  const [comments, setComments] = useState<object[]>([]);
+  const [inputName, setInputName] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [modal, setModal] = useState("")
 
   const users = new UserStore();
 
@@ -15,11 +19,11 @@ export default function Dashboard() {
     router.back();
   };
 
-  const getComments = async() => {
-    const data = await users.getComments()
-    setComments(data)
-    console.log(comments);
-  }
+  const getCommentsDB = async () => {
+    const data = await getComments();
+    setComments(data.response);
+    setModal("flex")
+  };
 
   const findUserByName = () => {
     const username = window.prompt("Enter the username to search on the list");
@@ -31,24 +35,27 @@ export default function Dashboard() {
     return users.findByName(username, usersRegistered);
   };
 
-  const createNewUser = () => {
-    const userName = window.prompt("Enter the username");
-    const userPassword = window.prompt("Enter the password");
-
-    if (!userName || !userPassword) {
-      return `Error creating the new user, enter a valid username and password`;
-    }
-
-    users.createUser(userName, userPassword, usersRegistered);
-    return `User ${userName} has been added to the list of users.`;
-  };
-
   const updateUser = () => {
     return users.updateUser(usersRegistered);
   };
 
   const removeUser = () => {
     return users.removeUser(usersRegistered);
+  };
+
+  // -------------------------------
+  const saveFormData = async () => {
+    const newName = inputName;
+    const newText = inputText;
+
+    const res = await createComment(newName, newText);
+
+    setTimeout(() => {
+      setInputName("");
+      setInputText("");
+    }, 1000);
+
+    return res;
   };
 
   return (
@@ -60,21 +67,15 @@ export default function Dashboard() {
         <div className="flex flex-col gap-4">
           <button
             className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg transition-all"
-            onClick={getComments}
+            onClick={getCommentsDB}
           >
-            Get Users
+            Get Comments
           </button>
           <button
             className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg transition-all"
             onClick={() => alert(`HTTP METHOD GET: ${findUserByName()}`)}
           >
             Find by Name
-          </button>
-          <button
-            className="bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 rounded-lg transition-all"
-            onClick={() => alert(`HTTP METHOD POST: ${createNewUser()}`)}
-          >
-            Create User
           </button>
           <button
             className="bg-purple-500 hover:bg-purple-600 text-white py-2 px-4 rounded-lg transition-all"
@@ -95,7 +96,40 @@ export default function Dashboard() {
         >
           Back
         </button>
+
+        <div className="mt-8 border-[1px] p-5 rounded-[.5rem]">
+          <h2 className="text-center mb-5">Create A New Comment</h2>
+          <div className="form__group flex flex-col gap-3">
+            <input
+              onChange={(e) => setInputName(e.target.value)}
+              className="border-2 p-2 rounded-[.3rem] border-gray-400"
+              type="text"
+              placeholder="Name..."
+              value={inputName}
+            />
+            <input
+              onChange={(e) => setInputText(e.target.value)}
+              className="border-2 border-gray-400 p-2 rounded-[.3rem]"
+              type="text"
+              placeholder="Text..."
+              value={inputText}
+            />
+
+            <button
+              onClick={async () => console.log(await saveFormData())}
+              className="bg-orange-500 text-white p-3 rounded-[.6rem] cursor-pointer"
+            >
+              Create Comment
+            </button>
+          </div>
+          <div className={`${modal ? `${modal} items-center justify-center fixed bg-gray-500/70 text-white top-0 left-0 w-full h-full text-[1rem]` : "hidden"}`}>
+            <div className="fixed top-3 right-3 cursor-pointer p-2 bg-black text-white" onClick={() => setModal("hidden")}>X</div>
+            <div className="p-3 bg-white border-2 text-black rounded-[.4rem]">
+              <p>Content Here</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
--- src/services/comments.ts
+++ src/services/comments.ts
@@ -2,7 +2,27 @@ import axios from "axios"
 
 const API_URL = 'http://localhost:3000/api/comments';
 
+// GET HTTP REQUEST
 export const getComments = async() => {
   const response = await axios.get(API_URL)
   return response.data
+}
+
+// POST HTTP REQUEST
+export const createComment = async(name:string, text:string) => {
+  try {
+    const newComment = {
+      name: name,
+      text: text
+    }
+  
+    const response = await axios.post(API_URL, newComment)
+    if (!response.data.ok) {
+      throw new Error("Error, cannot create the comment")
+    } else {
+      return response.data
+    }
+  } catch(e) {
+    alert(e)
+  }
 }
