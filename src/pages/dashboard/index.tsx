f606a5b8484278226c55c587157d8b70ab546914
ProgramaConPid <programaconpid@gmail.com>
2025-10-03 13:56:40 -0500

    Removed use from dashboard component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { use, useState } from "react";
+import { useState } from "react";
 import {
   getProperties,
   createProperty,
